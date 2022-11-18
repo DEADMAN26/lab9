f62355b0b962c542de87889fa53d433aa3a82e7f
Lenovo <Lenovo@10.12.16.102>
2022-11-18 14:34:06 +0530

    final commit

--- src/jUnitTestpackage/jUnitfunctions.java
+++ src/jUnitTestpackage/jUnitfunctions.java
@@ -1,5 +1,10 @@
 package jUnitTestpackage;
 
 public class jUnitfunctions {
-
+	public int addNumbers(int n1, int n2) {
+		return n1 + n2;
+	}
+	public String addStrings(String s1, String s2) {
+		return s1 + s2;
+	}
 }
